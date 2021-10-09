a5bbf2ecfbb5bbf07a4d85bd584cfa87b5d35643
A_ito <BoxPistols@users.noreply.github.com>
2021-10-09 19:15:47 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import React, { useState } from "react";
-import "./styles.scss";
+import React, { useState } from 'react'
+import { ChildArea } from './component/ChildArea'
+import './styles.scss'
 
 export default function App() {
   // counter state
-  const [count, setCount] = useState(100);
+  const [count, setCount] = useState(100)
+  const [text, setText] = useState('')
+  const [open, setOpen] = useState(true)
 
   const countUp = () => {
-    setCount((prev) => count + 2);
-  };
+    setCount((prev) => count + 2)
+  }
 
   const countDown = () => {
-    setCount((prev) => count - 2);
-  };
+    setCount((prev) => count - 2)
+  }
+
+  // form change
+  const onChangeInput = (e) => setText(e.target.value)
+
+  // Open Toggle
+  const onClickOpen = () => setOpen(!open)
+  console.log(open)
 
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>{count}</h2>
-      <button onClick={countUp}>countUp</button>
-      <button onClick={countDown}>countDown</button>
+      <div class="container">
+        <div class="row">
+          <div class="column column-80">
+            <h1>Hello Re:React</h1>
+            <section className="section">
+              {/* Counter */}
+              <h2>{count}</h2>
+              <button onClick={countUp}>countUp</button>
+              <button onClick={countDown}>countDown</button>
+            </section>
+          </div>
+        </div>
+        <div class="row">
+          <div class="column column-80">
+            <section className="section">
+              {/* Form */}
+              {text}
+              <input onChange={onChangeInput} value={text} />
+              <button class="button button-outline" onClick={onClickOpen}>
+                View Child
+              </button>
+              <ChildArea open={open} />
+            </section>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
