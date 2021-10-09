74b779b528f0b9e98fe918fb26412385e5871420
A_ito <BoxPistols@users.noreply.github.com>
2021-10-09 12:35:55 +0900

    Counter useState (prev) =>関数を1回挟んで直接定数を変更しないようにすべき（公式がおかしい説）
    setCount((prev) => count + 2);

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  // counter state
   const [count, setCount] = useState(100);
 
   const countUp = () => {
     setCount((prev) => count + 2);
+  };
+
+  const countDown = () => {
+    setCount((prev) => count - 2);
   };
 
   return (
@@ -13,6 +18,7 @@ export default function App() {
       <h1>Hello CodeSandbox</h1>
       <h2>{count}</h2>
       <button onClick={countUp}>countUp</button>
+      <button onClick={countDown}>countDown</button>
     </div>
   );
 }
